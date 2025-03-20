256c5b6eb01f481bc19d3adab22ed02489f73c81
kmcquil <kmcquillan449@gmail.com>
2025-03-20 14:06:36 -0400

    fix submit scripts

--- src/lakeflow_2_submit.sh
+++ src/lakeflow_2_submit.sh
@@ -3,15 +3,14 @@
 ###########################################################################
 ## environment & variable setup
 ####### job customization
-#SBATCH --job-name dwnld
+#SBATCH --job-name lkflw
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=5GB
-#SBATCH --time 5:00:00
+#SBATCH --time 1:00:00
 #SBATCH -p normal_q
 #SBATCH -A swot
-#SBATCH --array=1-2
 ####### end of job customization
 # end of environment & variable setup
 
@@ -20,4 +19,4 @@ apptainer exec \
     --pwd /projects/swot/kmcquil/LakeFlow_Confluence_Dev \
     --bind /projects/swot/kmcquil/LakeFlow_Confluence_Dev \
     --cleanenv \
-    /projects/swot/kmcquil/LakeFlow_Confluence_Dev/docker/lakeflow.sif Rscript src/lakeflow_1.R "in/lakeids/lakeid${SLURM_ARRAY_TASK_ID}.csv" 1
+    /projects/swot/kmcquil/LakeFlow_Confluence_Dev/docker/lakeflow.sif Rscript src/lakeflow_2.R "in/viable/lakeid1.csv" 6
